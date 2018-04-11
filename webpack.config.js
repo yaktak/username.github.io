a87fc0495e3318ce55bc5f4ccff82f8a0187e98f
yaktak <yakei034@gmail.com>
2018-04-12 00:28:23 +0900

    update: add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'development',
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: `./src/index.js`,
+
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: __dirname,
+    // 出力ファイル名
+    filename: 'app.js'
+  },
+
+  // ローカル開発用環境を立ち上げる
+  // 実行時にブラウザが自動的に localhost を開く
+  devServer: {
+    contentBase: __dirname,
+    open: true
+  }
+};
